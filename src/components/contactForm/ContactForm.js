ae74154a54c119ec356d51d6f3c7e6b5ec0bb40b
Matti Räty <mattiraty@hotmail.com>
2023-03-30 12:17:38 +0300

    created ContactForm -component

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,15 +1,25 @@
 import React from "react";
 
-export const ContactForm = ({
-  name,
-  setName,
-  phone,
-  setPhone,
-  email,
-  setEmail,
-  handleSubmit
-}) => {
+export const ContactForm = (props) => {
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    
+    if (target.id === 'name') {
+      props.setCurrentName(value);
+    } else if (target.id === 'phone') {
+      props.setCurrentPhone(value);
+    } else if (target.id === 'email') {
+      props.setCurrentEmail(value);
+    }
+  }
+
   return (
-    ContactForm
+    <form onSubmit={props.handleSubmit}>
+      <input type="text" id="name" placeholder="Enter name" value={props.currentName} onChange={handleChange} />
+      <input type="text" id="phone" placeholder="Enter phone" value={props.currentPhone} onChange={handleChange} />
+      <input type="text" id="email" placeholder="Enter email" value={props.currentEmail} onChange={handleChange} />
+      <button type='submit'>Submit</button>
+    </form>
   );
 };
