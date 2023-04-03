94be983c9484250a680862370594bbd11eeebc5c
Matti Räty <mattiraty@hotmail.com>
2023-04-03 14:33:30 +0300

    fixed issue from contact picker

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export const ContactPicker = (props) => {
   return (
-    <select onChange={props.setCurrentContact}>
+    <select onChange={(event) => props.setCurrentContact(event.target.value)}>
       <option>None</option>
       {props.contacts ? props.contacts.map(contact => {
-        return <option>{contact.name}</option>;
+        return <option key={contact.id}>{contact.name}</option>;
       }) : null}
     </select>
   );
